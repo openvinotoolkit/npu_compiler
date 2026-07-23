ed7d1e7b95a6446e5f329831658fd31e1c2990dc
nikolaygorb <nikolay.gorb@intel.com>
2026-07-23 11:18:19 +0000

    Revert "fix multiplication result converted to larger type #2782"
    This reverts commit 9f4f5a457cbc6262aab1569a03ee728dd072b64c.

--- src/vpux_compiler/src/dialect/VPU/utils/tile_utils.cpp
+++ src/vpux_compiler/src/dialect/VPU/utils/tile_utils.cpp
@@ -1253,7 +1253,7 @@ Byte getRequiredCMXSizeForDataPointerTable(mlir::Operation* op, int64_t OC) {
         const auto maxVariantsPerCluster =
                 static_cast<int32_t>(config::getConstraint(op, config::METADATA_MAX_VARIANT_COUNT) / 2);
 
-        requiredCMX += (maxDataPointerTableAlignment * 4_Byte) * maxVariantsPerCluster * maxClusters;
+        requiredCMX += Byte(maxClusters * maxVariantsPerCluster * (maxDataPointerTableAlignment * 4_Byte));
     } else {
         requiredCMX += Byte(maxClusters * (maxDataPointerTableAlignment * 4_Byte));
     }
